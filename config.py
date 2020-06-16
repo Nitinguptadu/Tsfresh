cb97d8dca602f12089a6edae6530b25eae60c472
Nitin <45600643+Nitinguptadu@users.noreply.github.com>
2020-06-16 19:33:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+input_file = 'data/data.csv'
+
+features_file = 'data/features.csv'
+
+train_file = 'data/train.csv'
+test_file = 'data/test.csv'
+
+validation_split_i = 1000
+
+# the bigger, the more features selected
+# default is 0.05
+fdr_level = 0.05				
